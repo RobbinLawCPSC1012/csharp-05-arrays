6dceaa4eef97e175c412b6a0416a2e924298cdfe
Robbin Law <robbinl@telusplanet.net>
2019-11-02 15:51:16 -0600

    a

--- 08-arrayofdoubles-AnalyzeNumbers/Program.cs
+++ 08-arrayofdoubles-AnalyzeNumbers/Program.cs
@@ -10,6 +10,39 @@ namespace _08_arrayofdoubles_AnalyzeNumbers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\tNumber Analyzer\n");
+            Console.Write("Enter number of items: ");
+            int size = int.Parse(Console.ReadLine());
+
+            double[] arr = new double[size];
+            double sum = 0;
+
+            // Get Each number for the array from the user.
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Enter a number: ");
+                arr[i] = double.Parse(Console.ReadLine());
+                sum += arr[i];
+            }
+
+            double avg = sum / size;
+            Console.WriteLine($"Num Elements: {size} Average: {avg}");
+
+            double min = arr[0];
+            double max = arr[0];
+
+            double bigCount = 0;
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] > avg)
+                    bigCount++;
+                if (arr[i] < min)
+                    min = arr[i];
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+            Console.WriteLine($"Number of Elements larger than average: {bigCount}");
+            Console.WriteLine($"MaxElt: {max} minElt: {min}");
         }
     }
 }
--- 28-2018-09-IC-arrays-methods-StudentGrades/Program.cs
+++ 28-2018-09-IC-arrays-methods-StudentGrades/Program.cs
@@ -10,6 +10,133 @@ namespace _28_2018_09_IC_arrays_methods_StudentGrades
     {
         static void Main(string[] args)
         {
+            int maxStudents = 5;
+            int[] grades = new int[maxStudents];
+
+            int numStudents = EnterGrades(grades, maxStudents);
+            SortGradesDescending(grades, numStudents);
+
+            DisplayArray(grades, numStudents);
+        }
+
+        static int EnterGrades(int[] grades, int max)
+        {
+            int count = 0;
+            for (int i = 0; i <= max; i++)
+            {
+                int num = 0;
+                bool validInput = false;
+                while (!validInput)
+                {
+                    if (count == max)
+                    {
+                        Console.WriteLine("-----------------------");
+                        Console.WriteLine($"The free edition supports a max of {max} grades.");
+                        Console.WriteLine("Please upgrade to a full edition for unlimited number of grades.");
+                        Console.WriteLine("-----------------------");
+                        Console.WriteLine("Press ANY key to continue.");
+                        Console.ReadLine();
+                        return count;
+                    }
+                    num = GetPositiveInt("Enter Grade: ");
+
+                    // Check for Exit Conditions
+                    if (num == 999)
+                    {
+                        return count;
+                    }
+
+                    if (num > 100)
+                    {
+                        Console.WriteLine("Invalid Value: Grade cannot be higher than 100.");
+                    }
+                    else
+                    {
+                        validInput = true;
+                    }
+                }
+                grades[i] = num;
+                count++;
+            }
+            return count;
+        }
+
+        /* Sort our Grades with SELECTION SORT */
+        static void SortGradesDescending(int[] grades, int size)
+        {
+            for (int scanIdx = 0; scanIdx < size - 1; scanIdx++)
+            {
+                int maxIdx = scanIdx;
+                for (int i = scanIdx + 1; i < size; i++)
+                {
+                    if (grades[i] > grades[maxIdx])
+                        maxIdx = i;
+                }
+
+                if (maxIdx != scanIdx)
+                {
+                    Swap(grades, scanIdx, maxIdx);
+                }
+            }
+        }
+
+        static void Swap(int[] arr, int a, int b)
+        {
+            int tmp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = tmp;
+        }
+
+        static void DisplayArray(int[] grades, int size)
+        {
+            Console.WriteLine("\nGrades from Highest to Lowest: ");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("\t " + grades[i]);
+            }
+            Console.WriteLine("Average: " + CalculateAverage(grades, size));
+        }
+
+        static double CalculateAverage(int[] grades, int size)
+        {
+            double sum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                sum += (double)grades[i];
+            }
+            return sum / size;
+        }
+
+        
+        static int GetPositiveInt(string msg)
+        {
+            try
+            {
+                Console.Write(msg);
+                int num = int.Parse(Console.ReadLine());
+                if (num < 0)
+                {
+                    Console.WriteLine("Invalid Input: Enter a non-negative value.");
+                    return GetPositiveInt(msg);
+                }
+                return num;
+            } catch(Exception ex)
+            {
+                Console.WriteLine("Invalid Input: Enter numbers. ");
+                return GetPositiveInt(msg);
+            }
+
+        }
+
+        static int GetPositiveInt(string msg, int max)
+        {
+            int num = GetPositiveInt(msg);
+            if (num > max)
+            {
+                Console.WriteLine($"Invalid Value: Enter number lower than {max}.");
+                GetPositiveInt(msg, max);
+            }
+            return num;
         }
     }
 }
