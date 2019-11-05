d0008ea2734fd5d2a7a74fe324c90b66e1feec2c
Robbin Law <robbinl@telusplanet.net>
2019-11-05 10:41:40 -0700

    a

--- 20-2017-09-IC4-arrays-methods-Reverse/Program.cs
+++ 20-2017-09-IC4-arrays-methods-Reverse/Program.cs
@@ -4,12 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _20_2017_09_IC4_arrays_methods_Reverse
+namespace _16_arrays_methods_AnalyzeNumbers
 {
     class Program
     {
+
+        static string[] GetWords()
+        {
+            Console.Write("Enter the number of words as an int: ");
+            int size = int.Parse(Console.ReadLine());
+
+            string[] arr = new string[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Enter next word");
+                arr[i] = Console.ReadLine();
+            }
+            return arr;
+        }
+
+        static void DisplayArrayElements(string[] stringArray)
+        {
+            Console.WriteLine("Number List:");
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                Console.Write($"{stringArray[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        static string[] ReverseArrayElements(string[] arr)
+        {
+            string[] reverseArray = new string[arr.Length];
+            int countDown = arr.Length - 1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                reverseArray[countDown] = arr[i];
+                countDown--;
+            }
+            return reverseArray;
+        }
+
         static void Main(string[] args)
         {
+            string[] wordArray = GetWords();
+            DisplayArrayElements(wordArray);
+            string[] reverseWordArray = ReverseArrayElements(wordArray);
+            DisplayArrayElements(reverseWordArray);
         }
     }
 }
