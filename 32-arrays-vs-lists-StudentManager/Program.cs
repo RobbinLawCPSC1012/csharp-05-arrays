8b4b3931c4fe5d57787f8e22ffabd0f34550c91f
Robbin Law <robbinl@telusplanet.net>
2019-11-11 11:18:56 -0700

    a

--- 32-arrays-vs-lists-StudentManager/Program.cs
+++ 32-arrays-vs-lists-StudentManager/Program.cs
@@ -8,13 +8,22 @@ namespace _32_arrays_vs_lists_StudentManager
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            withArrays();
+            withLists();
+        }
+
+        static void withArrays()
+        {
+            Console.WriteLine("*** Using traditional arrays where size must be statically set at declaration ***");
+
             Console.WriteLine("How many students in your class?");
             var studentCount = int.Parse(Console.ReadLine());
 
-            var studentNames = new string[studentCount];
-            var studentGrades = new int[studentCount];
+            string[] studentNames = new string[studentCount];
+            int[] studentGrades = new int[studentCount];
 
             for (int i = 0; i < studentCount; i++)
             {
@@ -28,6 +37,35 @@ namespace _32_arrays_vs_lists_StudentManager
             for (int i = 0; i < studentCount; i++)
             {
                 Console.WriteLine($"Name: {studentNames[i]}, Grade: {studentGrades[i]}");
+            }
+        }
+
+        static void withLists()
+        {
+            Console.WriteLine("*** Using dynamic arrays called lists where size is not set at declaration ***");
+
+            List<string> studentNames = new List<string>();
+            List<int> studentGrades = new List<int>();
+
+            var adding = true;
+
+            while(adding)
+            {
+                Console.Write("Student Name: ");
+                studentNames.Add(Console.ReadLine());
+
+                Console.Write("Student Grade: ");
+                studentGrades.Add(int.Parse(Console.ReadLine()));
+
+                Console.WriteLine("Add another? y/n");
+
+                if (Console.ReadLine() != "y")
+                    adding = false;
+            }
+
+            for (int i = 0; i < studentNames.Count; i++)
+            {
+                Console.WriteLine("Name: {0}, Grade: {1}", studentNames[i], studentGrades[i]);
             }
         }
     }
