64a4b237c4227efb060b34998a12f4cfc5f83e00
Robbin Law <robbinl@telusplanet.net>
2019-11-11 11:06:40 -0700

    a

---
+++ 32-arrays-vs-lists-StudentManager/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32_arrays_vs_lists_StudentManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many students in your class?");
+            var studentCount = int.Parse(Console.ReadLine());
+
+            var studentNames = new string[studentCount];
+            var studentGrades = new int[studentCount];
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.Write("Student Name: ");
+                studentNames[i] = Console.ReadLine();
+
+                Console.Write("Student Grade: ");
+                studentGrades[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine($"Name: {studentNames[i]}, Grade: {studentGrades[i]}");
+            }
+        }
+    }
+}
